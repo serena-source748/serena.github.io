6a268c5abc3da9bf3860a26027be15379cae3f54
serena-source748 <scariaserena@gmail.com>
2025-10-16 19:45:26 -0500

    Create hw2.js

---
+++ hw2.js
@@ -0,0 +1,21 @@
+ /*
+Name: Serena Scaria
+Date created: October 16, 2025
+Date last edited: October 16, 2025
+Version: 1.0
+Description: Homework 2 HTML Patient Registration Form
+*/
+
+//date code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+//household slider code
+let slider = document.getElementById("household");
+let output = document.getElementById("range-slider");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
