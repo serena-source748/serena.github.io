ca980d1b1d3c1b27fd7dc01bbd4cee6f458a8890
serena-source748 <scariaserena@gmail.com>
2025-12-01 15:35:25 -0600

    Update hw4.js

--- hw4.js
+++ hw4.js
@@ -293,6 +293,104 @@ function validateEverything() {
      }
     }
 
+//Cookie for remembering info input
+function setCookie(name, cvalue, expiryDays) {
+    var day = new Date();
+    day.setTime(day.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + day.toUTCString();
+    document.cookie = name + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(name) {
+    var cookieName = name + "+";
+    var cookies = document.cookie.split(';');
+
+    for (var i = 0; i < cookie.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) ==0) {
+        return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+ return "";
+}
+
+var inputs = [
+ {id:"fname", cookieName:"firstName"},
+ {id:"minitial", cookieName:"middleInitial"},
+ {id:"lname", cookieName:"lastName"},
+ {id:"dob", cookieName:"dob"},
+ {id:"ssn", cookieName:"ssn"},
+ {id:"address1", cookieName:"address1"},
+ {id:"city", cookieName:"city"},
+ {id:"zip", cookieName:"zipCode"},
+ {id:"email", cookieName:"email"},
+ {id:"phone", cookieName:"phone"},
+ {id:"uid", cookieName:"userId"}
+ ]
+
+inputs.forEach(function (input) {
+    var inputElement = document.getElementById(input.id);
+
+ // Prefill input fields
+ var cookieValue = getCookie(input.cookieName);
+ if (cookieValue !=="") {
+    inputElement.value = cookieValue;
+ }
+
+ // Set a cookie when the input field changes
+ inputElement.addEventListener("input", function() {
+     setCookie(input.cookieName, inputElement.value, 30);
+ });
+});
+
+//greet user with name
+var firstName = getCookie("firstName");
+if (firstName !== "") {
+    document.getElementById("welcome1").innerHTML = "Welcome back," + firstName + "!<br>";
+    document.getElementById("welcome2").innerHTML = "<a href='#' id='new-user'>Not" + firstName + "? Click here to start a new form.</a>";
+    document.getElementById("new-user").addEventListener("click", function() {
+       setCookie(input.cookieName, "", -1);
+      });
+   location.reload();
+  });
+}
+
+//Function to delete cookies
+function deleteAllCookies() {
+ document.cookie.split(";").forEach(function (cookie) {
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;";
+ });
+}
+
+//Remeber me checkbox validation
+document.getElementById("remember-me").addEventListener("change", function() {
+    const rememberMe = this.checked;
+
+    if(!rememberMe) {
+      //if "remember me" unchecked, delete cookies
+     deleteAllCookies();
+     console.log("All cookies deleted because 'Remember Me' is unchecked.");
+    } else {
+      // if "remember me" is checked or rechecked, save cookies
+     inputs.forEach(function(input) {
+        const inputElement = document.getElementById(input.id);
+        if (inputElement.value.trim() !== "") {
+           setCookie (input.cookieName, inputElement.value, 30);
+        }
+     });
+     console.log("Cookies saved because 'Remember Me' is checked.");
+    }
+});
+
+//Event listener for DOM Content Loaded
+document.addEventListener("DOMContentLoaded", function() {
+    const rememberMe = document.getElementById("remember-me").checked;
+    if (!rememberMe) {
+        deleteAllCookies();
+    }
+});
+
 //City validation
 function validateCity() {
     city = document.getElementById("city").value.trim();
